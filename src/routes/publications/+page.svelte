6037d950ae63343a69a6f1b812717f43a6c4cdf4
Jules Blanchet <j55blanchet@gmail.com>
2025-04-02 17:44:42 -0400

    Adding new publication

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -18,10 +18,19 @@ type Publication = {
 let publications = $state(
     [
         {
+            title: 'Enhancing the Educational Potential of Online Movement Videos: System Development and Empirical Studies with TikTok Dance Challenges',
+            year: 2025,
+            conference: 'CHI \'25: Proceedings of the 2025 CHI Conference on Human Factors in Computing Systems',
+            authors: ['Jules B. Blanchet', 'Megan E Hillis', 'Yeongji Lee', 'Qijia Shao', 'Xia Zhou', 'David JM Kraemer', 'Devin Balkcom'],
+            highlightAuthorIndex: 0,
+            doi: '10.1145/3706598.3714062',
+            artifacts: [],
+        },
+        {
             title: 'Integrating a LLM into an Automatic Dance Practice Support System: Breathing Life Into The Virtual Coach',
             year: 2023,
             conference: 'Adjunct Proceedings of the 36th Annual ACM Symposium on User Interface Software and Technology',
-            authors: ['Jules Blanchet', 'Sixuan Han'],
+            authors: ['J.B. Blanchet', 'Sixuan Han'],
             highlightAuthorIndex: 0,
             doi: '10.1145/3586182.3625119',
             artifacts: []
@@ -30,7 +39,7 @@ let publications = $state(
             title: 'LearnThatDance: Augmenting TikTok Dance Challenge Videos with an Interactive Practice Support System Powered by Automatically Generated Lesson Plans',
             year: 2023,
             conference: 'Adjunct Proceedings of the 36th Annual ACM Symposium on User Interface Software and Technology',
-            authors: ['Jules Blanchet', 'Megan E Hillis', 'Yeongji Lee', 'Qijia Shao', 'Xia Zhou', 'David JM Kraemer', 'Devin Balkcom'],
+            authors: ['J.B. Blanchet', 'Megan E Hillis', 'Yeongji Lee', 'Qijia Shao', 'Xia Zhou', 'David JM Kraemer', 'Devin Balkcom'],
             highlightAuthorIndex: 0,
             doi: '10.1145/3586182.3615801',
             artifacts: []
@@ -39,7 +48,7 @@ let publications = $state(
             title: 'Automatic Generation and Teaching of Dance Lessons from Video',
             year: 2023,
             conference: 'HotMobile \'23: Proceedings of the 24th International Workshop on Mobile Computing Systems and Applications',
-            authors: ['Jules Blanchet', 'Megan E Hillis', 'Yeongji Lee', 'Qijia Shao', 'Xia Zhou', 'David JM Kraemer', 'Devin Balkcom'],
+            authors: ['J.B. Blanchet', 'Megan E Hillis', 'Yeongji Lee', 'Qijia Shao', 'Xia Zhou', 'David JM Kraemer', 'Devin Balkcom'],
             highlightAuthorIndex: 0,
             doi: '10.1145/3572864.3581592',
             artifacts: []
@@ -48,7 +57,7 @@ let publications = $state(
             title: 'Soft Lattice Modules That Behave Independently and Collectively',
             year: 2022,
             conference: 'IEEE Robotics and Automation Letters',
-            authors: ['Luyang Zhao', 'Yijia Wu', 'Jules Blanchet', 'Maxine Perroni-Scharf', 'Xiaonan Huang', 'Joran Booth', 'Rebecca Kramer-Bottiglio', 'Devin Balkcom'],
+            authors: ['Luyang Zhao', 'Yijia Wu', 'J.B. Blanchet', 'Maxine Perroni-Scharf', 'Xiaonan Huang', 'Joran Booth', 'Rebecca Kramer-Bottiglio', 'Devin Balkcom'],
             highlightAuthorIndex: 2,
             doi: '10.1109/LRA.2022.3160611',
         },
@@ -56,7 +65,7 @@ let publications = $state(
             title: 'Overlapping semantic representations of sign and speech in novice sign language learners',
             year: 2022,
             conference: 'Proceedings of the Annual Meeting of the Cognitive Science Society',
-            authors: ['Megan E Hillis', 'Brianna Aubrey', 'Jules Blanchet', 'Qijia Shao', 'Xia Zhou', 'Devin Balkcom', 'David JM Kraemer'],
+            authors: ['Megan E Hillis', 'Brianna Aubrey', 'J.B. Blanchet', 'Qijia Shao', 'Xia Zhou', 'Devin Balkcom', 'David JM Kraemer'],
             highlightAuthorIndex: 2,
             nonDoiPaperLink: 'https://escholarship.org/uc/item/4bw8874x',
         },
@@ -64,7 +73,7 @@ let publications = $state(
             title: 'Teaching American Sign Language in Mixed Reality',
             year: 2020,
             conference: 'Proceedings of the ACM on Interactive, Mobile, Wearable and Ubiquitous Technologies',
-            authors: ['Qijia Shao', 'Amy Sniffen', 'Jules Blanchet', 'Megan E Hillis', 'Xinyu Shi', 'Themistoklis K Haris', 'Jason Liu', 'Jason Lamberton', 'Melissa Malzkuhn', 'Lorna C Quandt', 'James Mahoney', 'David JM Kraemer', 'Xia Zhou', 'Devin Balkcom'],
+            authors: ['Qijia Shao', 'Amy Sniffen', 'J.B. Blanchet', 'Megan E Hillis', 'Xinyu Shi', 'Themistoklis K Haris', 'Jason Liu', 'Jason Lamberton', 'Melissa Malzkuhn', 'Lorna C Quandt', 'James Mahoney', 'David JM Kraemer', 'Xia Zhou', 'Devin Balkcom'],
             highlightAuthorIndex: 2,
             doi: '10.1145/3432211',
         }
@@ -77,10 +86,10 @@ let publications = $state(
     <h1>Publications</h1>
     
 
-    <div class="alert not-prose">
+    <!-- <div class="alert not-prose">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
         <span><strong>Two</strong> additional papers are currently under review.</span>
-    </div>
+    </div> -->
 
     <div class="space-y-8 mt-8 not-prose">
     {#each publications as pub}
